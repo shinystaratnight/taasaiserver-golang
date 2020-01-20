00f5f97dd578c7338c8b84a5576a5a65a0a7f08b
Guruprasath <guruprasathb@outlook.com>
2020-01-20 16:13:58 +0530

    fixed bugs

--- models/RideEventLog.go
+++ models/RideEventLog.go
@@ -1,16 +1,14 @@
 package models
 
 import (
-	"time"
-
 	"github.com/jinzhu/gorm"
 )
 
 type RideEventLog struct {
 	gorm.Model
 
-	RideID   uint `gorm:"not null;index:idx_ride"`
-	RideStatus int64 `gorm:"not null"`
-	Message string `gorm:"not null"`
+	RideID     uint   `gorm:"not null;index:idx_ride"`
+	RideStatus int64  `gorm:"not null"`
+	Message    string `gorm:"not null"`
 	IsActive   bool
 }
